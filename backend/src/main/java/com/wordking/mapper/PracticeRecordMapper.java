48b07c1b5ad41bdc9ea566faab774017cb575585
Frank Xu <386277405@qq.com>
2026-01-28 22:14:13 +0800

    init project

--- backend/src/main/java/com/wordking/mapper/PracticeRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.wordking.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wordking.entity.PracticeRecord;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PracticeRecordMapper extends BaseMapper<PracticeRecord> {
-}
